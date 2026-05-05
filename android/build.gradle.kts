50231c14a452aea9bc0f2e552dbac4574c3bfb1f
rifky-raihan <mrifkyraihan1410@gmail.com>
2026-05-05 15:40:39 +0700

    perbaikan file build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 allprojects {
     repositories {
         google()
@@ -5,20 +7,31 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir: Directory = rootProject.layout.buildDirectory.dir(project.name).get()
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// --- FIX UNTUK ISAR & PLUGIN LAMA YANG TIDAK PUNYA NAMESPACE ---
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library")) {
+            configure<LibraryExtension> {
+                if (namespace == null) {
+                    namespace = "com.example." + project.name.replace("-", "_")
+                }
+            }
+        }
+    }
 }
